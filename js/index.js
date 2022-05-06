ed05f2a0fa6e3f1443807bc9f5f9fbaf15d76e79
Bipin Kumar <bipin.kumar@laminaar.in>
2022-05-06 13:12:19 +0530

    throttling logic working fine

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const loggerFunc = () => {
+  console.count("Throttled Function");
+};
+
+const throttle = (fn, limit) => {
+  let flag = true;
+  return function () {
+    let context = this;
+    let args = arguments;
+    if (flag) {
+      fn.apply(context, args);
+      flag = false;
+      setTimeout(() => {
+        flag = true;
+      }, limit);
+    }
+  };
+};
+
+const betterLoggerFunction = throttle(loggerFunc, 1000);
